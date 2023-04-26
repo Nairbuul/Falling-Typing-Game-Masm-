cecdb56de1d6972d8ad59ac72ccf3e9f70d2b19a
Luu, Brian <luu_brian@ymail.com>
2023-04-25 20:36:30 -0700

    Clearing

--- main.asm
+++ main.asm
@@ -4,69 +4,106 @@ INCLUDE Irvine32.inc
 INCLUDE Macros.inc
 
 .data
-	; define your variables here
-	line BYTE 0,11,0,"Hello World",0,50,2,0
-	textSize DWORD 0
-	text BYTE 20 DUP (?)
+    ; define your variables here
+    line BYTE 0,11,0,"Hello World",0,50,0,0
+    line2 BYTE 0,13,0, "Goodbye World", 0, 10,0,0
+    textSize DWORD 0
+    text BYTE 20 DUP (?)
+
+    loopCounter BYTE 0
 
 .code
 
 main PROC
-	inputs:
-		mov esi, OFFSET line
-		mov edi, esi
-		add esi, 3
-		add edi, 1
+    inputs:
+        inc [loopCounter]                      ;Decremnet counter...
+        mov al, [loopCounter]
+        cmp al, 10
+        jge decrement
 
-		movzx ecx, BYTE PTR[edi]
-		add esi, ecx
-		inc esi
-	
-	;-------------------------------
-	;COMMENT
-	;Printing original string
-	;-------------------------------
-		mov eax, white
-		call setTextColor
-		mGoToXY BYTE PTR [esi], BYTE PTR [esi+1]		;Arguments (x-position, y-position)
-		mWriteString OFFSET (line+3)					;Arguments (address location)
-	
-	;-------------------------------
-	;COMMENT
-	;Overlapping string with color matched inputs
-	;-------------------------------
-		mov eax, lightMagenta
-		call setTextColor
-		mGoToXY BYTE PTR [esi], BYTE PTR [esi+1]
-		mWriteString OFFSET (text)
+        mov esi, OFFSET line                  ;[esi] Start of variable string
+        mov edi, esi
+        add esi, 3
+        add edi, 1                          ;[edi] location to the string size
 
-		mov eax, 500
-		call delay
+        movzx ecx, BYTE PTR[edi]
+        add esi, ecx
+        inc esi
+    
+    ;-------------------------------
+    ;COMMENT
+    ;Printing original string
+    ;-------------------------------
+        mov eax, white
+        call setTextColor
+        mGoToXY BYTE PTR [esi], BYTE PTR [esi+1]        ;Arguments (x-position, y-position)
+        mWriteString OFFSET (line+3)                    ;Arguments (address location)
+    
+    ;-------------------------------
+    ;COMMENT
+    ;Overlapping string with color matched inputs
+    ;-------------------------------
+        mov eax, lightMagenta
+        call setTextColor
+        mGoToXY BYTE PTR [esi], BYTE PTR [esi+1]
+        mWriteString OFFSET (text)
 
-	;-------------------------------
-	;COMMENT 
-	;inputs
-	;need a way to make sure it matches the string.
-	;-------------------------------
-		call readKey
-		mov ebx, OFFSET text
-		mov edx, textSize
-		add ebx, edx
-		mov edx, edi
-		add edx, 2
-		add edx, [textSize]
-		cmp al, [edx]
-		jne here
-			mov [ebx], al
-			inc [textSize]
-		here:
+        mov eax, 50
+        call delay
 
-		call clrscr
-		inc BYTE PTR [esi+1]
-		
-		cmp al, 27d					;If escape key we leave
-		jne inputs
+    ;-------------------------------
+    ;COMMENT 
+    ;inputs
+    ;need a way to make sure it matches the string.
+    ;-------------------------------
+        call readKey
+        mov ebx, OFFSET text
+        mov edx, textSize
+        add ebx, edx
+        mov edx, edi
+        add edx, 2
+        add edx, [textSize]
+        cmp al, [edx]
+        jne here
+            mov [ebx], al
+            inc [textSize]
+            mov bl, BYTE PTR [textsize]
+            cmp bl, cl
+            jb here
+                ;clear variable / get next Variable???
+                call clearVariable
+                mov BYTE PTR [(edi+2)], 0
+        here:
 
-	INVOKE ExitProcess, 0
+        call clrscr
+        
+        cmp al, 27d                    ;If escape key we leave
+        jne inputs
+        je EndOfProgram
+
+    Decrement: 
+         mov [loopCounter], 0
+        inc BYTE PTR [esi+1]
+        jmp inputs
+
+    EndOfProgram:
+    INVOKE ExitProcess, 0
 main ENDP
+
+clearVariable PROC
+    push esi
+    mov ecx, 5
+    mov esi, OFFSET text
+
+    Clearing:
+        mov DWORD PTR [esi], 0
+        add esi, DWORD
+     loop Clearing
+
+     mov [textSize], 0
+     pop esi
+
+    ret
+clearVariable ENDP 
+
 END main
